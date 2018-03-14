705d22301deebe6e364cd284be0c8df831e8a801
Joey_nii <joey110119@gmail.com>
2018-03-15 00:09:48 +0800

    can put Marker on the map.

--- app/src/main/java/com/example/r_ni/maptest/MapsActivity.java
+++ app/src/main/java/com/example/r_ni/maptest/MapsActivity.java
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import android.app.Activity;
@@ -42,14 +43,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MapsActivity extends Activity{
+public class MapsActivity extends Activity implements OnMapReadyCallback{
 
-    /**
-     * 主 变量
-     */
+    ///////////the thing of server///////////
+    /*主 变量*/
     // 主线程Handler
     // 用于将从server获取的消息显示出来
     private Handler mMainHandler;
@@ -61,9 +62,7 @@ public class MapsActivity extends Activity{
     // 为了方便展示,此处直接采用线程池进行线程管理,而没有一个个开线程
     private ExecutorService mThreadPool;
 
-    /**
-     * 接收server消息 变量
-     */
+    /*接收server消息 变量*/
     // 输入流对象
     InputStream is;
 
@@ -76,34 +75,51 @@ public class MapsActivity extends Activity{
     private JSONObject json_read;
     public static JSONArray myjs_array;
 
-    /**
-     * 发送消息到server 变量
-     */
-    // 输出流对象
-    OutputStream outputStream;
-
-    Fragment mapFragment;
+    //////////////the things of map///////////////
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        MapFragment mapFrag;
+        final MapFragment mapfragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        mapfragment.getMapAsync(this);
+    }
 
-        mapFragment = MapFragment.newInstance();
-        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-        fragmentTransaction.add(R.id.map, mapFragment);
-        fragmentTransaction.commit();
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney and move the camera
+        server_connect();
+        mMap = googleMap;
+        LatLng sydney = new LatLng(23.973875, 120.982024);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        setUpMap();
+    }
 
+    // 移動地圖到參數指定的位置
+    private void moveMap(LatLng place) {
+        // 建立地圖攝影機的位置物件
+        CameraPosition cameraPosition =
+                new CameraPosition.Builder()
+                        .target(place)
+                        .zoom(7)
+                        .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    private void setUpMap() {
+        // 建立位置的座標物件
+        LatLng place = new LatLng(23.973875, 120.982024);
+        // 移動地圖
+        moveMap(place);
+    }
+
+    private void server_connect(){
         // 初始化线程池
         mThreadPool = Executors.newCachedThreadPool();
 
-        // 实例化主线程,用于更新接收过来的消息
-        mMainHandler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-
-            }
-        };
 
         /**
          * 创建客户端 & server的连接
@@ -114,7 +130,7 @@ public class MapsActivity extends Activity{
             public void run() {
                 try {
                     // 创建Socket对象 & 指定服务端的IP 及 端口号
-                    socket = new Socket("192.168.137.246", 9999);
+                    socket = new Socket("172.20.10.5", 9999);
                     // 判断客户端和服务器是否连接成功
                     System.out.println(socket.isConnected());
 
@@ -157,14 +173,43 @@ public class MapsActivity extends Activity{
 
             }
         });
+
+        // 实例化主线程,""用于更新接收过来的消息""
+        mMainHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                if(response!=null){
+                    try{
+                        myjs_array  = new JSONArray(response);
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(),"response is null", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                }
+
+                JSONObject myjObject = null;
+                try {
+                    myjObject = myjs_array.getJSONObject(0);
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(),"jsonarray[0] is null", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+
+                try {
+                    String lat = myjObject.getString("gps_lat");
+                    String lon = myjObject.getString("gps_lon");
+                    float f_lat = Float.parseFloat(lat);
+                    float f_lon = Float.parseFloat(lon);
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(f_lat,f_lon)));
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(),"can't find that index", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+
+            }
+        };
     }
 
-    /*public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }*/
 }
